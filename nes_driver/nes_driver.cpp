5c17e795b6d4a3a9650e69474e7d400ef262acdb
Chuck Ries <Chuck.Ries@microsoft.com>
2016-03-16 10:45:59 -0700

    Various cleanup and fixes to improve GDI rendering

--- nes_driver/nes_driver.cpp
+++ nes_driver/nes_driver.cpp
@@ -6,6 +6,10 @@
 typedef unsigned char u8;
 #include <nesapi.h>
 
+#include <chrono>
+
+#include <thread>
+
 #include <Windows.h>
 #include <objidl.h>
 #include <gdiplus.h>
@@ -16,14 +20,16 @@ class GdiGfx : public IGfx, public IInput
 {
 public:
     GdiGfx()
+        : _pBitmap(nullptr)
+        , _lastDrawTime(std::chrono::high_resolution_clock::now())
     {
         GdiplusStartup(&_gdiplusToken, &_gdiplusStartupInput, NULL);
 
         HINSTANCE hInstance = GetModuleHandle(NULL);
 
         RECT clientRect = {};
-        clientRect.bottom = 240;
-        clientRect.right = 256;
+        clientRect.bottom = 240 * 3;
+        clientRect.right = 256 * 3;
         AdjustWindowRect(&clientRect, (WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX), false);
 
         WNDCLASS wndclass = {};
@@ -43,7 +49,7 @@ public:
             NULL,                     // parent window handle
             NULL,                     // window menu handle
             nullptr,                // program instance handle
-            NULL);
+            this);
 
         ShowWindow(_hWnd, SW_SHOW);
         UpdateWindow(_hWnd);
@@ -61,6 +67,15 @@ public:
             delete _pBitmap;
         }
         _pBitmap = new Bitmap(256, 240, 3 * 256, PixelFormat24bppRGB, screen);
+
+        std::chrono::time_point<std::chrono::steady_clock> now;
+        long long duration;
+        do
+        {
+            now = std::chrono::high_resolution_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::nanoseconds>(now - _lastDrawTime).count();
+        } while (duration < 16666667); // 60 fps
+        _lastDrawTime = now;
 
         RedrawWindow(_hWnd, NULL, NULL, RDW_INVALIDATE | RDW_UPDATENOW);
     }
@@ -93,14 +108,15 @@ private:
 
     HWND _hWnd;
 
-    static Bitmap* _pBitmap;
-    static JoypadState _joypadState;
+    Bitmap* _pBitmap;
+    JoypadState _joypadState;
+    std::chrono::time_point<std::chrono::high_resolution_clock> _lastDrawTime;
 
-    static void OnPaint(HWND hWnd, HDC hdc)
+    void OnPaint(HWND hWnd, HDC hdc)
     {
         if (_pBitmap != nullptr)
         {
-            Graphics graphics(hdc);
+            Gdiplus::Graphics graphics(hdc);
 
             RECT rect;
             GetClientRect(hWnd, &rect);
@@ -117,43 +133,72 @@ private:
         HDC hdc;
         PAINTSTRUCT ps;
 
+        GdiGfx* pThis;
+
+        if (message == WM_CREATE)
+        {
+            pThis = static_cast<GdiGfx*>(reinterpret_cast<CREATESTRUCT*>(lParam)->lpCreateParams);
+
+            SetLastError(0);
+            if (!SetWindowLongPtr(hWnd, GWL_USERDATA, reinterpret_cast<LONG_PTR>(pThis)))
+            {
+                if(GetLastError() != 0)
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            pThis = reinterpret_cast<GdiGfx*>(GetWindowLongPtr(hWnd, GWL_USERDATA));
+        }
+
         switch (message)
         {
         case WM_PAINT:
-            hdc = BeginPaint(hWnd, &ps);
-            OnPaint(hWnd, hdc);
-            EndPaint(hWnd, &ps);
-            return 0;
+            if (pThis)
+            {
+                hdc = BeginPaint(hWnd, &ps);
+                pThis->OnPaint(hWnd, hdc);
+                EndPaint(hWnd, &ps);
+                return 0;
+            }
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
         case WM_KEYDOWN:
+        {
+            JoypadState& joypad = pThis->_joypadState;
             switch (wParam)
             {
-            case 0x53: _joypadState.A = true; break;
-            case 0x41: _joypadState.B = true; break;
-            case VK_RSHIFT: _joypadState.Select = true; break;
-            case VK_RETURN: _joypadState.Start = true; break;
-            case VK_UP: _joypadState.Up = true; break;
-            case VK_DOWN: _joypadState.Down = true; break;
-            case VK_LEFT: _joypadState.Left = true; break;
-            case VK_RIGHT: _joypadState.Right = true; break;
+            case 0x53: joypad.A = true; break;
+            case 0x41: joypad.B = true; break;
+            case VK_LSHIFT: case VK_SHIFT: case VK_RSHIFT: joypad.Select = true; break;
+            case VK_RETURN: joypad.Start = true; break;
+            case VK_UP: joypad.Up = true; break;
+            case VK_DOWN: joypad.Down = true; break;
+            case VK_LEFT: joypad.Left = true; break;
+            case VK_RIGHT: joypad.Right = true; break;
             default: return DefWindowProc(hWnd, message, wParam, lParam);
             }
+        }
             return 0;
         case WM_KEYUP:
+        {
+            JoypadState& joypad = pThis->_joypadState;
             switch (wParam)
             {
-            case 0x53: _joypadState.A = false; break;
-            case 0x41: _joypadState.B = false; break;
-            case VK_RSHIFT: _joypadState.Select = false; break;
-            case VK_RETURN: _joypadState.Start = false; break;
-            case VK_UP: _joypadState.Up = false; break;
-            case VK_DOWN: _joypadState.Down = false; break;
-            case VK_LEFT: _joypadState.Left = false; break;
-            case VK_RIGHT: _joypadState.Right = false; break;
+            case 0x53: joypad.A = false; break;
+            case 0x41: joypad.B = false; break;
+            case VK_LSHIFT: case VK_SHIFT: case VK_RSHIFT: joypad.Select = false; break;
+            case VK_RETURN: joypad.Start = false; break;
+            case VK_UP: joypad.Up = false; break;
+            case VK_DOWN: joypad.Down = false; break;
+            case VK_LEFT: joypad.Left = false; break;
+            case VK_RIGHT: joypad.Right = false; break;
             default: return DefWindowProc(hWnd, message, wParam, lParam);
             }
+        }
             return 0;
         default:
             return DefWindowProc(hWnd, message, wParam, lParam);
@@ -161,8 +206,18 @@ private:
     }
 };
 
-Bitmap* GdiGfx::_pBitmap = nullptr;
-JoypadState GdiGfx::_joypadState;
+void RunNes(const char* romPath)
+{
+    //SdlGfx* sdlGfx = SdlGfx_Create(3);
+    GdiGfx gdiGfx;
+
+    Nes* nes = Nes_Create(romPath, (IGfx*)&gdiGfx, (IInput*)&gdiGfx);
+    if (nes != nullptr)
+        Nes_Run(nes);
+
+    //SdlGfx_Destroy(sdlGfx);
+    Nes_Destroy(nes);
+}
 
 int main(int argc, char* argv[])
 {
@@ -172,13 +227,18 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    SdlGfx* sdlGfx = SdlGfx_Create(3);
-    GdiGfx gdiGfx;
+    std::thread nesThread(RunNes, argv[1]);
 
-    Nes* nes = Nes_Create(argv[1], (IGfx*)&gdiGfx, (IInput*)&gdiGfx);
-    if (nes != nullptr)
-        Nes_Run(nes);
+    nesThread.join();
 
-    SdlGfx_Destroy(sdlGfx);
-    Nes_Destroy(nes);
+    //SdlGfx* sdlGfx = SdlGfx_Create(3);
+    //GdiGfx gdiGfx1;
+    //GdiGfx gdiGfx2;
+
+    //Nes* nes1 = Nes_Create(argv[1], (IGfx*)&gdiGfx1, (IInput*)&gdiGfx1);
+    //if (nes1 != nullptr)
+    //    Nes_Run(nes1);
+
+    ////SdlGfx_Destroy(sdlGfx);
+    //Nes_Destroy(nes1);
 }
